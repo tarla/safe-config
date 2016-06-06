87b8c34346621d7057f6f9a2435b29c660b094d3
João Tarla <joao@tarla.com.br>
2016-06-06 02:47:31 -0300

    Fetches environment vars

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 'use strict';
-console.log(require('./lib/safe-config')(__dirname+'/conf/application.conf'));
+var config = require('./lib/safe-config')(__dirname+'/conf/application.conf');
+console.log(config);
+console.log(config.a);
+console.log(config.env);
+console.log(config['env.home']);
